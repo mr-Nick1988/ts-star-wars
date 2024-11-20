b81e16277a93532ceac33894ff40ccbce8527112
Nick <andreeffnick2@gmail.com>
2024-11-20 13:48:44 +0200

    first ts star-wars commit

---
+++ src/utils/types.d.ts
@@ -0,0 +1,21 @@
+export interface SWContextValue{
+    page:string;
+    changePage:(page:string)=>void,
+}
+
+
+
+export interface heroInfo{
+    name: string,
+    gender: string,
+    birth_year:string,
+    height: number,
+    mass: number,
+    hair_color: string,
+    skin_color: string,
+    eye_color: string
+}
+
+//export interface heroInfo{
+   // [key:string]:string;
+//}
